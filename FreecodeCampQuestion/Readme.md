836ee1cd32c5a10f14b503e754106dbd48ad32b2
NITISH KUMAR ROY <31863867+ntishroy92@users.noreply.github.com>
2023-09-27 10:32:41 +0000

    function

---
+++ FreecodeCampQuestion/Readme.md
@@ -0,0 +1,2 @@
+function declaration is a simple function that does not requiure any variable to store and the function have a name.
+ Function expression is a anynomous function that is assigned to a variable. 
---
+++ FreecodeCampQuestion/function.js
@@ -0,0 +1,13 @@
+//   Function Expression
+var subs = function(a,b){
+    console.log("Subtract :",a-b);
+  }
+  subs(5,4);
+
+
+// Function Declaration
+function add( a,b){
+    console.log("Sum : ",a+b);
+ }
+  add(4,5);
+
